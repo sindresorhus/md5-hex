f89bd6a67155f70c691fc45b74d5430aa3a26fdb
Sindre Sorhus <sindresorhus@gmail.com>
2015-06-04 22:30:02 +0200

    forgot to define encoding

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 var crypto = require('crypto');
 
 module.exports = function (buf) {
-	return crypto.createHash('md5').update(buf).digest('hex');
+	return crypto.createHash('md5').update(buf, 'utf8').digest('hex');
 };
