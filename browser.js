41ef8965fb40ad2e79535a3d3daa341eec5f79bf
Kevin Mårtensson <kevinmartensson@gmail.com>
2016-04-12 16:23:27 +0200

    Join array in browser (#8)
    Fixes #3.

--- browser.js
+++ browser.js
@@ -1,2 +1,10 @@
 'use strict';
-module.exports = require('md5-o-matic');
+var md5OMatic = require('md5-o-matic');
+
+module.exports = function (input) {
+	if (Array.isArray(input)) {
+		input = input.join('');
+	}
+
+	return md5OMatic(input);
+};
