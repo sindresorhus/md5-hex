5ceed005014af669f13c01fc005eb45bb2133f81
Hongarc <anhhong12121997t@gmail.com>
2019-05-03 22:05:57 +0700

    Lint with xo

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import md5Hex from './';
+import md5Hex from '.';
 
 test('main', t => {
 	t.is(md5Hex('unicorn'), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.is(md5Hex(new Buffer('unicorn')), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.is(md5Hex(Buffer.from('unicorn')), '1abcb33beeb811dca15f0ac3e47b88d9');
 	t.is(md5Hex(['uni', 'corn']), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.is(md5Hex(['uni', new Buffer('corn')]), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.is(md5Hex(['uni', Buffer.from('corn')]), '1abcb33beeb811dca15f0ac3e47b88d9');
 	t.throws(() => md5Hex('uni', 'corn'), 'Too many arguments. Try specifying an array.');
 });
