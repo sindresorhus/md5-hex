926c7b23a6b3bc7e0564ab4a35d31122a0655bd7
Hongarc <aqua.hong.97@gmail.com>
2019-05-04 15:19:14 +0700

    Meta tweaks (#11)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import m from './';
+import md5Hex from '.';
 
-test(t => {
-	t.is(m('unicorn'), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.is(m(new Buffer('unicorn')), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.is(m(['uni', 'corn']), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.is(m(['uni', new Buffer('corn')]), '1abcb33beeb811dca15f0ac3e47b88d9');
-	t.throws(() => m('uni', 'corn'), 'Too many arguments. Try specifying an array.');
+test('main', t => {
+	t.is(md5Hex('unicorn'), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.is(md5Hex(Buffer.from('unicorn')), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.is(md5Hex(['uni', 'corn']), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.is(md5Hex(['uni', Buffer.from('corn')]), '1abcb33beeb811dca15f0ac3e47b88d9');
+	t.throws(() => md5Hex('uni', 'corn'), 'Too many arguments. Try specifying an array.');
 });
