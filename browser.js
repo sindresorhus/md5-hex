b64f5d184a2abe0f2e002890d1f0650d60ff7924
Aditya Patadia <aditya@patadia.org>
2019-07-11 21:22:51 +0530

    added test

--- browser.js
+++ browser.js
@@ -1,10 +1,10 @@
 'use strict';
-const blueimp_md5 = require('blueimp-md5');
+const blueimpMd5 = require('blueimp-md5');
 
 module.exports = data => {
 	if (Array.isArray(data)) {
 		data = data.join('');
 	}
 
-	return blueimp_md5(data);
+	return blueimpMd5(data);
 };
--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 import test from 'ava';
 import md5Hex from '.';
 
+const md5HexBrowser = require('./browser');
+
 test('main', t => {
 	t.is(md5Hex('unicorn'), '1abcb33beeb811dca15f0ac3e47b88d9');
 	t.is(md5Hex(Buffer.from('unicorn')), '1abcb33beeb811dca15f0ac3e47b88d9');
 	t.is(md5Hex(['uni', 'corn']), '1abcb33beeb811dca15f0ac3e47b88d9');
 	t.is(md5Hex(['uni', Buffer.from('corn')]), '1abcb33beeb811dca15f0ac3e47b88d9');
+});
+
+test('chinese character md5 matches', t => {
+	t.is(md5Hex('中文chinese'), md5HexBrowser('中文chinese'));
 });
