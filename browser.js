0419affdab37bacf933c8e33011606fca2f8837d
Aditya Patadia <aditya@patadia.org>
2019-07-11 18:26:25 +0530

    fixes chinese character hex in browser

--- browser.js
+++ browser.js
@@ -1,10 +1,10 @@
 'use strict';
-const md5OMatic = require('md5-o-matic');
+const blueimp_md5 = require('blueimp-md5');
 
 module.exports = data => {
 	if (Array.isArray(data)) {
 		data = data.join('');
 	}
 
-	return md5OMatic(data);
+	return blueimp_md5(data);
 };
