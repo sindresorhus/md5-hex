b4b30c368186ae3a9a1fb5b447e65d0ce7a331e3
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-09 05:16:25 +0200

    Meta tweaks

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,10 +1,10 @@
 import {expectType} from 'tsd';
-import * as fs from 'node:fs';
+import fs from 'node:fs';
 import md5Hex from './index.js';
 
 const buffer = fs.readFileSync('unicorn.png');
 
-expectType<string>(md5Hex(buffer));
+expectType<string>(md5Hex(buffer)); // eslint-disable-line @typescript-eslint/no-unsafe-argument
 expectType<string>(md5Hex([buffer]));
 expectType<string>(md5Hex('foo'));
 expectType<string>(md5Hex(['foo']));
